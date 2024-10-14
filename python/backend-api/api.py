80b24628b99371aa667571f473c6a7bf996ce370
Bartek <bartek.bak@protonmail.com>
2024-10-14 16:18:43 +0100

    Fixed the parsing of the tasks.txt

--- python/backend-api/api.py
+++ python/backend-api/api.py
@@ -1,14 +1,28 @@
+import json
 from flask import Flask
 
 app = Flask(__name__)
 
-file = open("./tasks.txt")
-tasks_array = file.read()
-print(tasks_array)
+with open("./tasks.txt", 'r') as file:
+    try:
+        tasks_array = json.load(file)
+    except json.JSONDecodeError:
+        print("Error: Invalid JSON format in tasks.txt")
+        tasks_array = []
+
 
 @app.route("/read/<index>")
 def return_tasks(index):
-    return tasks_array[index]
+    try:
+        index = int(index)
+        if 0 <= index < len(tasks_array):
+            return tasks_array[index]
+        else:
+            return "Index out of range", 400
+    except ValueError:
+        return "Invalid index", 400
 
+@app.route("/write/<index>", methods=["POST"])
+def write_tasks(index):
 
 app.run(host="0.0.0.0", port=5001)
