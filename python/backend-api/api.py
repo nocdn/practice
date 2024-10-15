4169b8670df0334cec6c0aa3f93a57b18f8d90c8
Bartek <bartek.bak@protonmail.com>
2024-10-15 12:29:06 +0100

    Fixed issue with writing to file

--- python/backend-api/api.py
+++ python/backend-api/api.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 with open("./tasks.txt", 'r') as file:
     tasks_array = json.load(file)
 
+def write_to_file(data_to_write):
+    with open("./tasks.txt", 'w') as file:
+        json.dump(data_to_write, file)
+
+tasks_array.append("breakdance")
+write_to_file(tasks_array)
 
 @app.route("/read/<index>")
 def return_tasks(index):
@@ -18,7 +24,7 @@ def return_tasks(index):
     except ValueError:
         return "Invalid index", 400
 
-@app.route("/write/<index>", methods=["POST"])
-def write_tasks(index):
+#@app.route("/write/<index>", methods=["POST"])
+#def write_tasks(index):
 
 app.run(host="0.0.0.0", port=5001)
