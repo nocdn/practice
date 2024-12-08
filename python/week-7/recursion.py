0f24e820065c9d88c17be30ca5883fb004d4582e
Bartek Bak <bartek.bak@protonmail.com>
2024-12-08 14:04:38 +0000

    Continued with recursion

--- python/week-7/recursion.py
+++ python/week-7/recursion.py
@@ -24,3 +24,20 @@ def recursive_is_palindrome(word):
         return recursive_is_palindrome(word[1:-1])
     
 # print(recursive_is_palindrome("racecar"))
+
+def sum_digits(number):
+    if len(number) <= 1:
+        return number
+    # to implement
+
+
+def merge_lists(list1, list2):
+    final_list = []
+    for item in list1:
+        final_list.append(item)
+    for item in list2:
+        final_list.append(item)
+    final_list.sort()
+    return final_list
+
+print(merge_lists([4, 7, 1, 8, 9], [5, 5, 9, 1, 3, 1, 6, 4]))
